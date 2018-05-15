71cb59c039af9d1755d54dccb6df5711e48d18dc
MLGC00KIE <24893@ma-web.nl>
2018-05-15 23:09:55 +0200

    added placeholder background - working movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float speedForward;
+
+    [SerializeField]
+    private float speedBackward;
+
+    [SerializeField]
+    private float dodgeSpeed;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            
+            Debug.Log("forward");
+            transform.Translate(Vector3.right * Time.deltaTime * speedForward);
         }
 
-	}
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("backwards");
+            transform.Translate(Vector3.left * Time.deltaTime * speedBackward);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("forward");
+            transform.Translate(Vector3.up * Time.deltaTime * dodgeSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("backwards");
+            transform.Translate(Vector3.down * Time.deltaTime * dodgeSpeed);
+        }
+
+    }
 }
