c349b00c3df4b31356bdc00ac40824eff3c09a37
MLGC00KIE <24893@ma-web.nl>
2018-05-16 12:54:31 +0200

    added movement - added shooting script

---
+++ Assets/Scripts/Player/Attacks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacks : MonoBehaviour {
+
+    [SerializeField]
+    private float bulletSpeed = 10f;
+    [SerializeField]
+    private Rigidbody bullet;
+
+    // fire the bullet
+    void Fire()
+    {
+        Vector3 bulletpos = (transform.position + new Vector3(0,0,0.2f));
+
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, bulletpos, transform.rotation);
+        bulletClone.velocity = transform.right * bulletSpeed;
+        Physics.IgnoreCollision(bulletClone.GetComponent<Collider>(), GetComponent<Collider>(), true);
+        //GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Fire();
+        }
+
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
-    private float speedForward;
+    private float thrust;
 
     [SerializeField]
-    private float speedBackward;
+    private float dodgeThrust;
 
     [SerializeField]
-    private float dodgeSpeed;
+    private float reverseThrust;
+
+    public Rigidbody rb;
+
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
     // Use this for initialization
     void Start () {
@@ -21,29 +29,57 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        if (Input.GetKey(KeyCode.D))
+        // DIRECT MOVEMENT
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Debug.Log("forward");
+        //    transform.Translate(Vector3.right * Time.deltaTime * speedForward);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    Debug.Log("backwards");
+        //    transform.Translate(Vector3.left * Time.deltaTime * speedBackward);
+        //}
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    Debug.Log("forward");
+        //    transform.Translate(Vector3.up * Time.deltaTime * dodgeSpeed);
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    Debug.Log("backwards");
+        //    transform.Translate(Vector3.down * Time.deltaTime * dodgeSpeed);
+        //}
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("forward");
-            transform.Translate(Vector3.right * Time.deltaTime * speedForward);
+            rb.AddForce(transform.up * dodgeThrust);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("backwards");
-            transform.Translate(Vector3.left * Time.deltaTime * speedBackward);
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("forward");
-            transform.Translate(Vector3.up * Time.deltaTime * dodgeSpeed);
+            rb.AddForce(-transform.right * reverseThrust);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("backwards");
-            transform.Translate(Vector3.down * Time.deltaTime * dodgeSpeed);
+            rb.AddForce(-transform.up * dodgeThrust);
         }
 
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * thrust);
+        }
+
+            rb.velocity = rb.velocity * 0.9f;
     }
 }
