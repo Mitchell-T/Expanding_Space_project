d6dd736f4ec93237f404fefbf2191090feaf7b13
MLGC00KIE <24893@ma-web.nl>
2018-05-17 17:31:36 +0200

    added 1 pattern for enemy shooting

---
+++ Assets/Scripts/Bullets/MoveForward.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+
+    [SerializeField]
+    private float Velocity;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.up * Time.deltaTime * Velocity;
+    }
+}
--- Assets/Scripts/Player/Attacks.cs
+++ Assets/Scripts/Player/Attacks.cs
@@ -9,16 +9,7 @@ public class Attacks : MonoBehaviour {
     [SerializeField]
     private Rigidbody bullet;
 
-    // fire the bullet
-    void Fire()
-    {
-        Vector3 bulletpos = (transform.position + new Vector3(0,0,0.2f));
-
-        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, bulletpos, transform.rotation);
-        bulletClone.velocity = transform.right * bulletSpeed;
-        Physics.IgnoreCollision(bulletClone.GetComponent<Collider>(), GetComponent<Collider>(), true);
-        //GetComponent<AudioSource>().Play();
-    }
+    
 
     // Update is called once per frame
     void Update () {
@@ -28,4 +19,16 @@ public class Attacks : MonoBehaviour {
         }
 
 	}
+
+
+    // fire the bullet
+    void Fire()
+    {
+        Vector3 bulletpos = (transform.position + new Vector3(0, 0, 0.2f));
+
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, bulletpos, transform.rotation);
+        bulletClone.velocity = transform.right * bulletSpeed;
+        Physics.IgnoreCollision(bulletClone.GetComponent<Collider>(), GetComponent<Collider>(), true);
+        //GetComponent<AudioSource>().Play();
+    }
 }
