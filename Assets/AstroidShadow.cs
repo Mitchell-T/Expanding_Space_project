af59dc5891c42bd218df72c9e4b201672a8d8838
PeterSchreuder <peter.schreuder@quicknet.nl>
2018-05-28 11:04:45 +0200

    Aan de boss area verder gewerkt (niet af, boss glitchey)

---
+++ Assets/AstroidShadow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidShadow : MonoBehaviour {
+
+    public Transform objectA;
+    public Transform objectB;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Make ObjectA's position match objectB
+        objectA.position = objectB.position;
+
+        //Now parent the object so it is always there
+        //objectA.parent = objectB;
+    }
+}
